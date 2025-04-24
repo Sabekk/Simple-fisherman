60bcb5dc1aa1af914682943a063e5b6d136dd14a
Sabek <5255050+Sabekk@users.noreply.github.com>
2025-04-24 23:53:34 +0200

    Added floater

---
+++ Assets/Code/Gameplay/FIshing/Floater.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floater : MonoBehaviour
+{
+    #region VARIABLES
+
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float depthBeforeSubmerged = 1f;
+    [SerializeField] private float displacementAmount = 3f;
+    [SerializeField] private float rotationSpeed = 2f;
+
+    float closesDistance = 0;
+
+    #endregion
+
+    #region PROPERTIES
+
+    #endregion
+
+    #region UNITY_METHODS
+
+    private void FixedUpdate()
+    {
+        if (CheckIsFloating(out Transform transformHit) && transformHit != null)
+        {
+            float displacementMultipler = Mathf.Clamp01(transformHit.position.y - transform.position.y / depthBeforeSubmerged) * displacementAmount;
+            rb.AddForce(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultipler, 0f), ForceMode.Acceleration);
+        }
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(Vector3.zero), rotationSpeed * Time.deltaTime);
+    }
+
+    #endregion
+
+    #region METHODS
+
+    private bool CheckIsFloating(out Transform transformHit)
+    {
+        transformHit = null;
+
+        Ray ray = new Ray(transform.position + Vector3.up * 10f, Vector3.down);
+        RaycastHit[] hits = Physics.RaycastAll(ray, 15f);
+
+        foreach (RaycastHit hit in hits)
+        {
+            if (hit.transform == transform) continue;
+
+            transformHit = hit.transform;
+            Debug.Log(transformHit.name);
+            return true;
+        }
+
+        return false;
+    }
+
+    #endregion
+}
